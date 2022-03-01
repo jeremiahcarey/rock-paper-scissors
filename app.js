a209803715ab413f104f3b75147bfd2d19b0225e
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2022-03-01 14:31:32 -0500

    more or less finished game

--- app.js
+++ app.js
@@ -14,24 +14,43 @@ function playRound(playerSelection, computerSelection) {
         return `You both played ${playerSelection}.  It's a tie!`;
     }
     else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        computerScore += 1;
         return 'Paper beats rock.  You lose!';
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        playerScore += 1;
         return 'Rock beats scissors.  You win!';
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        playerScore += 1;
         return 'Paper beats rock.  You win!';
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        computerScore += 1;
         return 'Scissors beats paper.  You lose!';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        computerScore += 1;
         return 'Rock beats scissors.  You lose!';
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        playerScore += 1;
         return 'Scissors beats paper.  You win!';
     } else {
         return 'Invalid play.  Try again.';
     }
 }
 
-    // function game() {
-    //     const playerScore = 0;
-    //     const computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    // }
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Rock, paper, or scissors? Enter your play below. (We\'ll play five rounds.)');
+        console.log(playRound(playerSelection, computerPlay()));
+    }
+    if (playerScore === computerScore) {
+        console.log(`We tied! ${playerScore} apiece.`);
+    } else if (playerScore > computerScore) {
+        console.log(`You won this match! ${playerScore} to ${computerScore}`);
+    } else {
+        console.log(`I won this time. ${computerScore} to ${playerScore}`);
+    }
+    playerScore = 0;
+    computerScore = 0;
+}
