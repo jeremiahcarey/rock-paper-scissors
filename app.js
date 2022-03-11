a629395ed69abaf8203ba8b53865a00d73df26fd
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2022-03-10 23:22:42 -0500

    layout and logic mmostly done - need to make it stop at five

--- app.js
+++ app.js
@@ -1,56 +1,94 @@
+const playerButtons = document.querySelectorAll('.player-option');
+const allButtons = document.querySelectorAll('.play-button');
+const computerRockBtn = document.querySelector('#computer-rock');
+const computerPaperBtn = document.querySelector('#computer-paper');
+const computerScissorsBtn = document.querySelector('#computer-scissors');
+const scorePlayer = document.querySelector('#you-score');
+const scoreComputer = document.querySelector('#computer-score');
+const results = document.querySelector('#print-results');
+
 function computerPlay() {
     const play = Math.floor(Math.random() * 3 + 1);
     if (play === 1) {
+        computerRockBtn.classList.add('played');
         return 'rock';
     } else if (play === 2) {
+        computerPaperBtn.classList.add('played');
         return 'paper';
     } else {
+        computerScissorsBtn.classList.add('played');
         return 'scissors';
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
-        return `You both played ${playerSelection}.  It's a tie!`;
-    }
-    else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        computerScore += 1;
-        return 'Paper beats rock.  You lose!';
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        playerScore += 1;
-        return 'Rock beats scissors.  You win!';
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        playerScore += 1;
-        return 'Paper beats rock.  You win!';
-    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        computerScore += 1;
-        return 'Scissors beats paper.  You lose!';
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-        computerScore += 1;
-        return 'Rock beats scissors.  You lose!';
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        playerScore += 1;
-        return 'Scissors beats paper.  You win!';
-    } else {
-        return 'Invalid play.  Try again.';
     }
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Rock, paper, or scissors? Enter your play below. (We\'ll play five rounds.)');
-        console.log(playRound(playerSelection, computerPlay()));
-    }
-    if (playerScore === computerScore) {
-        console.log(`We tied! ${playerScore} apiece.`);
-    } else if (playerScore > computerScore) {
-        console.log(`You won this match! ${playerScore} to ${computerScore}`);
-    } else {
-        console.log(`I won this time. ${computerScore} to ${playerScore}`);
-    }
-    playerScore = 0;
-    computerScore = 0;
+function updateScores() {
+    scorePlayer.innerText = `  ${playerScore}`;
+    scoreComputer.innerText = `  ${computerScore}`;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        results.innerText = `You both chose ${playerSelection}. It's a tie!`;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore += 1;
+        results.innerText = 'Paper beats rock.  You lose!';
+        updateScores();
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore += 1;
+        results.innerText = 'Rock beats scissors.  You win!';
+        updateScores();
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore += 1;
+        results.innerText = 'Paper beats rock.  You win!';
+        updateScores();
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore += 1;
+        results.innerText = 'Scissors beats paper.  You lose!';
+        updateScores();
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerScore += 1;
+        results.innerText = 'Rock beats scissors.  You lose!';
+        updateScores();
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore += 1;
+        updateScores();
+        results.innerText = 'Scissors beats paper.  You win!';
+    }
+}
+
+
+
+function undoPlayed() {
+    allButtons.forEach(button => button.classList.remove('played'));
+}
+
+playerButtons.forEach(button => button.addEventListener('click', function (e) {
+    e.target.parentElement.classList.add('played');
+    playRound(e.target.id, computerPlay());
+    const undoPlayDelay = setTimeout(undoPlayed, 2500);
+}));
+
+
+// function game() {
+//     while (computerScore < 5 && playerScore < 5)
+// }
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt('Rock, paper, or scissors? Enter your play below. (We\'ll play five rounds.)');
+//         console.log(playRound(playerSelection, computerPlay()));
+//     }
+//     if (playerScore === computerScore) {
+//         console.log(`We tied! ${playerScore} apiece.`);
+//     } else if (playerScore > computerScore) {
+//         console.log(`You won this match! ${playerScore} to ${computerScore}`);
+//     } else {
+//         console.log(`I won this time. ${computerScore} to ${playerScore}`);
+//     }
+//     playerScore = 0;
+//     computerScore = 0;
+// }
